cce5231dbe26c5ae24f53ad23754a04e70ca4303
atiyo <atiyo@atiyo>
2019-12-10 21:33:47 +0000

    Memory inefficient Repa version

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,11 @@
-{-# LANGUAGE Strict #-}
 module Main where
-import qualified Data.Map as M
---import qualified Data.Map.Strict as M
+import qualified Data.Array.Repa as R
 import qualified Codec.Picture as P
+import qualified Data.Map as M
 import Data.Maybe
 
-sideLength = 512 :: Int
-h = 0.037 :: Double
+sideLength = 64 :: Int
+h = 0.046 :: Double
 reps = 8 
 side = take sideLength [0..]
 validInds = [(x, y) | x <- side, y <- side]
@@ -15,26 +14,19 @@ validInds = [(x, y) | x <- side, y <- side]
 type H = Double
 type Histogram = M.Map (Int, Int) Double
 type Point = (Double, Double)
-type Points = [Point]
+type Points = R.Array R.D R.DIM1 Point
 type View = P.Image P.Pixel8
-type System = (Points, Histogram)
 type Ind = (Int, Int)
-type Inds = [Ind]
-
-
-initHistogram :: Histogram
-initHistogram = M.fromList keyValues
-  where inds = (,) <$> side <*> side
-        keyValues = [(key, 0.0) | key <- inds]
+type Inds = R.Array R.D R.DIM1 Ind
+type System = (Points, Inds)
 
 initPoints :: Points
-initPoints = values
+initPoints = R.fromFunction (R.Z R.:. numPoints) f
   where stepSize = 2 * pi / fromIntegral sideLength
         singleDim = take sideLength [-pi, -pi + stepSize..]
         values = (,) <$> singleDim <*> singleDim
-
-initSystem :: System
-initSystem = (initPoints, initHistogram)
+        numPoints = sideLength ^ 2
+        f (R.Z R.:. x) = values !! x
 
 updatePoint :: H -> Point -> Point
 updatePoint h (x, y) = (newX, newY)
@@ -42,28 +34,29 @@ updatePoint h (x, y) = (newX, newY)
         newY = y - h * sin (x + tan (3 * x))
 
 updatePoints :: H -> Points -> Points
-updatePoints param  = map $ updatePoint param
+updatePoints param  = R.map $ updatePoint param
 
 pointToInd :: Point -> Ind
 pointToInd (x, y) = (toInd x,  toInd y)
   where toInd z = floor $ fromIntegral sideLength * (z + pi) / (2 * pi)
 
 pointsToInds :: Points -> Inds
-pointsToInds = map pointToInd
+pointsToInds = R.map pointToInd
 
-countInds :: Inds -> Histogram
-countInds inds = M.fromListWith (+) tupled
-  where valid x = x `elem` validInds
-        tupled = [(ind, 1.0) | ind <- inds, valid ind]
+updateSystems :: H -> [System] -> [System]
+updateSystems h (x:xs) = (newPoints, newInds):x:xs
+  where newPoints = updatePoints h $ fst x
+        newInds = pointsToInds newPoints
+updateSystems h [] = [(initPoints, pointsToInds initPoints)]
 
-updateHistogram :: Histogram -> Histogram -> Histogram
-updateHistogram = M.unionWith (+)
+simulate :: H -> Int -> [System]
+simulate h n = updateSystems h $ simulate h (n - 1)
 
-updateSystem :: H -> System -> System
-updateSystem param (points, hist) = (newPoints, newHist)
-  where newPoints = updatePoints param points
-        incHist = countInds (pointsToInds newPoints)
-        newHist = updateHistogram hist incHist
+generateHist :: [System] -> Histogram
+generateHist systems = M.fromListWith (+) values
+  where listDelayed = map snd systems
+        listComputed = concatMap R.toList listDelayed
+        values = [(value, 1.0) | value <- listComputed]
 
 histToImage :: Histogram -> P.Image P.Pixel8
 histToImage hist = P.generateImage f sideLength sideLength
@@ -71,15 +64,11 @@ histToImage hist = P.generateImage f sideLength sideLength
         val x y = fromMaybe 0 $ M.lookup (x, y) hist
         f x y = floor $ 255 * val x y / maxValue
 
-writeOutput :: System -> FilePath -> IO ()
-writeOutput (_, hist) filePath = P.saveBmpImage filePath $ P.ImageY8 image
-  where image = histToImage hist
-
-repFun :: (a -> a) -> Int -> a -> a
-repFun f n a
-  | n > 1 = f $ repFun f (n - 1) a
-  | otherwise = f a
+writeOutput :: [System] -> FilePath -> IO ()
+writeOutput systems filePath = P.saveBmpImage filePath $ P.ImageY8 image
+  where image = histToImage . generateHist $ systems
 
 main :: IO ()
+--main = putStrLn "yo!"
 main = writeOutput final "./test.bmp"
-  where final = repFun (updateSystem h) reps initSystem 
+  where final = simulate h reps
