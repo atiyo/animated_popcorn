84646f50bbcaaf50e8abc06433af3e0a9dc6d07d
atiyo <atiyo@atiyo>
2019-12-18 14:18:46 +0000

    Unoptimised ST Monad implementation

--- src/Main.hs
+++ src/Main.hs
@@ -1,74 +1,102 @@
 module Main where
-import qualified Data.Array.Repa as R
-import qualified Codec.Picture as P
-import qualified Data.Map as M
+import Data.Array.Base
+import Data.Array.Unboxed
+import Data.Array.ST
 import Data.Maybe
+import Control.Monad
+import Control.Monad.ST
+import Codec.Picture
 
-sideLength = 64 :: Int
+sideLength = 128 :: Int
 h = 0.046 :: Double
-reps = 8 
-side = take sideLength [0..]
-validInds = [(x, y) | x <- side, y <- side]
+reps = 8
+maxInd = sideLength ^ 2 - 1
+numPoints = sideLength ^ 2
 
 
 type H = Double
-type Histogram = M.Map (Int, Int) Double
-type Point = (Double, Double)
-type Points = R.Array R.D R.DIM1 Point
-type View = P.Image P.Pixel8
 type Ind = (Int, Int)
-type Inds = R.Array R.D R.DIM1 Ind
-type System = (Points, Inds)
+type Histogram = UArray Ind Double
+type Point = (Double, Double)
+type Points = (UArray Int Double, UArray Int Double)
+type System = (Points, Histogram)
+
 
 initPoints :: Points
-initPoints = R.fromFunction (R.Z R.:. numPoints) f
+initPoints = (makeCoords xs, makeCoords ys)
   where stepSize = 2 * pi / fromIntegral sideLength
         singleDim = take sideLength [-pi, -pi + stepSize..]
         values = (,) <$> singleDim <*> singleDim
-        numPoints = sideLength ^ 2
-        f (R.Z R.:. x) = values !! x
+        xs = [fst value | value <- values]
+        ys = [snd value | value <- values]
+        makeCoords = listArray (0, maxInd)
 
-updatePoint :: H -> Point -> Point
-updatePoint h (x, y) = (newX, newY)
+initHistogram :: Histogram
+initHistogram = runSTUArray $ newArray ((0, 0), (maxInd, maxInd)) 0
+  
+
+updatePoint :: Monad m =>  H -> Point -> m Point
+updatePoint h (x, y) = return (newX, newY)
   where newX = x - h * sin (y + tan (3 * y))
         newY = y - h * sin (x + tan (3 * x))
 
 updatePoints :: H -> Points -> Points
-updatePoints param  = R.map $ updatePoint param
+updatePoints h points =  runST $ do
+  let (xs, ys) =  points
+  thawX <- thaw xs
+  thawY <- thaw ys
+  forM_ (take numPoints [0..]) $ \i -> do
+    oldX <- readArray thawX i
+    oldY <- readArray thawY i
+    (newX, newY) <- updatePoint h (oldX, oldY)
+    writeArray thawX i newX
+    writeArray thawY i newY
+  liftM2 (,) (unsafeFreezeSTUArray thawX) (unsafeFreezeSTUArray thawY)
 
-pointToInd :: Point -> Ind
-pointToInd (x, y) = (toInd x,  toInd y)
-  where toInd z = floor $ fromIntegral sideLength * (z + pi) / (2 * pi)
+pointToInd :: Monad m => Point -> m Ind
+pointToInd (x, y) = return (toInd x,  toInd y)
+  where norm z = mod z maxInd
+        toInt z = floor $ fromIntegral sideLength * (z + pi) / (2 * pi)
+        toInd = norm . toInt
+    
+updateHistogram :: Histogram -> Points -> Histogram
+updateHistogram hist points = runSTUArray $ do
+  let (xs, ys) =  points
+  --thawX <- thaw xs
+  --thawY <- thaw ys
+  thawHist <- thaw hist
+  forM_ (take numPoints [0..]) $ \i -> do
+    let xVal = xs ! i
+    let yVal = ys ! i
+    newInd <- pointToInd (xVal, yVal)
+    currentVal <- readArray thawHist newInd
+    writeArray thawHist newInd (currentVal + 1)
+  --unsafeFreezeSTUArray thawX
+  --unsafeFreezeSTUArray thawY
+  return thawHist
+       
+updateSystem :: H -> System -> System
+updateSystem h (points, hist) = (newPoints, newHist)
+  where newPoints = updatePoints h points
+        newHist = updateHistogram hist newPoints
 
-pointsToInds :: Points -> Inds
-pointsToInds = R.map pointToInd
+simulate :: H -> Int -> System
+simulate h n
+  | n > 0 = updateSystem h $ simulate h (n - 1)
+  | otherwise = (initPoints, initHistogram)
 
-updateSystems :: H -> [System] -> [System]
-updateSystems h (x:xs) = (newPoints, newInds):x:xs
-  where newPoints = updatePoints h $ fst x
-        newInds = pointsToInds newPoints
-updateSystems h [] = [(initPoints, pointsToInds initPoints)]
-
-simulate :: H -> Int -> [System]
-simulate h n = updateSystems h $ simulate h (n - 1)
-
-generateHist :: [System] -> Histogram
-generateHist systems = M.fromListWith (+) values
-  where listDelayed = map snd systems
-        listComputed = concatMap R.toList listDelayed
-        values = [(value, 1.0) | value <- listComputed]
-
-histToImage :: Histogram -> P.Image P.Pixel8
-histToImage hist = P.generateImage f sideLength sideLength
-  where maxValue = M.foldr max 0 hist
-        val x y = fromMaybe 0 $ M.lookup (x, y) hist
+histToImage :: Histogram -> Image Pixel8
+histToImage hist = generateImage f sideLength sideLength
+  where maxValue = foldl max 0 $ elems hist
+        val x y = hist ! (x, y)
         f x y = floor $ 255 * val x y / maxValue
 
-writeOutput :: [System] -> FilePath -> IO ()
-writeOutput systems filePath = P.saveBmpImage filePath $ P.ImageY8 image
-  where image = histToImage . generateHist $ systems
+writeOutput :: Histogram -> FilePath -> IO ()
+writeOutput hist filePath = saveBmpImage filePath $ ImageY8 image
+  where image = histToImage hist
 
 main :: IO ()
 --main = putStrLn "yo!"
-main = writeOutput final "./test.bmp"
-  where final = simulate h reps
+main = do
+  let (_, hist) = simulate h reps
+  writeOutput hist "./test.bmp"
