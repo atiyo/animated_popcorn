fcd76f5b6cfc733d61eb34210fdb4a1d0bad00d1
atiyo <atiyo@atiyo>
2019-12-18 22:01:32 +0000

    First fastish ST Monad implementation

--- src/Main.hs
+++ src/Main.hs
@@ -7,16 +7,17 @@ import Control.Monad
 import Control.Monad.ST
 import Codec.Picture
 
-sideLength = 128 :: Int
-h = 0.046 :: Double
-reps = 8
+sideLength = 1024 :: Int
+h = 0.023 :: Double
+reps = 128
 maxInd = sideLength ^ 2 - 1
 numPoints = sideLength ^ 2
 
 
 type H = Double
-type Ind = (Int, Int)
-type Histogram = UArray Ind Double
+type Ind = Int
+type Coord = (Int, Int)
+type Histogram = UArray Int Double
 type Point = (Double, Double)
 type Points = (UArray Int Double, UArray Int Double)
 type System = (Points, Histogram)
@@ -32,11 +33,11 @@ initPoints = (makeCoords xs, makeCoords ys)
         makeCoords = listArray (0, maxInd)
 
 initHistogram :: Histogram
-initHistogram = runSTUArray $ newArray ((0, 0), (maxInd, maxInd)) 0
+initHistogram = runSTUArray $ newArray (0, numPoints - 1) 0
   
 
-updatePoint :: Monad m =>  H -> Point -> m Point
-updatePoint h (x, y) = return (newX, newY)
+updatePoint :: H -> Point -> Point
+updatePoint h (x, y) = (newX, newY)
   where newX = x - h * sin (y + tan (3 * y))
         newY = y - h * sin (x + tan (3 * x))
 
@@ -48,32 +49,39 @@ updatePoints h points =  runST $ do
   forM_ (take numPoints [0..]) $ \i -> do
     oldX <- readArray thawX i
     oldY <- readArray thawY i
-    (newX, newY) <- updatePoint h (oldX, oldY)
+    (newX, newY) <- return $ updatePoint h (oldX, oldY)
     writeArray thawX i newX
     writeArray thawY i newY
   liftM2 (,) (unsafeFreezeSTUArray thawX) (unsafeFreezeSTUArray thawY)
 
-pointToInd :: Monad m => Point -> m Ind
-pointToInd (x, y) = return (toInd x,  toInd y)
-  where norm z = mod z maxInd
+pointToCoord :: Point -> Coord
+pointToCoord (x, y) = (toInd x,  toInd y)
+  where norm z = mod z sideLength
         toInt z = floor $ fromIntegral sideLength * (z + pi) / (2 * pi)
         toInd = norm . toInt
+
+coordToInd :: Coord -> Ind
+coordToInd (x, y) = x * sideLength + y
+
+indToCoord :: Ind -> Coord
+indToCoord i = (major, i - major * sideLength)
+  where major = floor $ fromIntegral i / fromIntegral sideLength
     
 updateHistogram :: Histogram -> Points -> Histogram
-updateHistogram hist points = runSTUArray $ do
+updateHistogram hist points = runST $ do
   let (xs, ys) =  points
-  --thawX <- thaw xs
-  --thawY <- thaw ys
+  thawX <- thaw xs
+  thawY <- thaw ys
   thawHist <- thaw hist
   forM_ (take numPoints [0..]) $ \i -> do
-    let xVal = xs ! i
-    let yVal = ys ! i
-    newInd <- pointToInd (xVal, yVal)
+    xVal <- readArray thawX i
+    yVal <- readArray thawY i
+    newInd <- return (coordToInd . pointToCoord $ (xVal, yVal))
     currentVal <- readArray thawHist newInd
     writeArray thawHist newInd (currentVal + 1)
-  --unsafeFreezeSTUArray thawX
-  --unsafeFreezeSTUArray thawY
-  return thawHist
+  _ <- unsafeFreezeSTUArray thawX
+  _ <- unsafeFreezeSTUArray thawY
+  unsafeFreezeSTUArray thawHist
        
 updateSystem :: H -> System -> System
 updateSystem h (points, hist) = (newPoints, newHist)
@@ -87,16 +95,16 @@ simulate h n
 
 histToImage :: Histogram -> Image Pixel8
 histToImage hist = generateImage f sideLength sideLength
-  where maxValue = foldl max 0 $ elems hist
-        val x y = hist ! (x, y)
-        f x y = floor $ 255 * val x y / maxValue
+  where norm = sum (elems hist) / fromIntegral numPoints
+        val x y = hist ! coordToInd (x, y)
+        f x y = floor $ 255 * val x y / norm
 
 writeOutput :: Histogram -> FilePath -> IO ()
-writeOutput hist filePath = saveBmpImage filePath $ ImageY8 image
+writeOutput hist filePath = saveJpgImage 95 filePath $ ImageY8 image
   where image = histToImage hist
 
 main :: IO ()
 --main = putStrLn "yo!"
 main = do
   let (_, hist) = simulate h reps
-  writeOutput hist "./test.bmp"
+  writeOutput hist "./test.jpg"
